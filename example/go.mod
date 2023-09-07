94f27ea6774c1a545af88b57312b1042c196987a
Brendan Burns <bburns@microsoft.com>
2023-09-07 20:53:48 +0000

    Add an example

---
+++ example/go.mod
@@ -0,0 +1,5 @@
+module github.com/brendandburns/caddy-wasm/example
+
+go 1.21.1
+
+require github.com/dev-wasm/dev-wasm-go/http v0.0.0-20230803142009-0dee5e3d3722 // indirect
---
+++ example/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/dev-wasm/dev-wasm-go/http/server"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("This is the alpha release!"))
+	})
+	server.ListenAndServe(nil)
+}

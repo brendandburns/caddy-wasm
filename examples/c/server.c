01b7d60b4e052475276233f3210f877e83a5243b
Brendan Burns <bburns@microsoft.com>
2024-03-15 18:57:24 +0000

    Move to the dev-wasm C library

--- examples/c/server.c
+++ examples/c/server.c
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 
+#define bool int
+
 void handler_fn(wasi_http_request_t *req, wasi_http_response_t *res) {
     res->status_code = 200;
     sprintf(res->body, "{ \"msg\": \"Hello world!\", \"path\": \"%s\", \"authority\": \"%s\"}\n", req->path_query, req->authority);
